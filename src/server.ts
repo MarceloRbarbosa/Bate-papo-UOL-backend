78a51c4800a393134893c2ef769168e1067f0c06
Marcelo R. Barbosa <marceloribeirobarbosa92@gmail.com>
2026-02-05 16:55:06 -0300

    feat(user): Controller && Prisma

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import chalk from "chalk";
 import dotenv from "dotenv";
 dotenv.config();
 import app from "./app";
@@ -5,5 +6,5 @@ import app from "./app";
 
 const port = process.env.PORT || 7000;
 app.listen(port, () => 
-    console.log(`Server is up and running on ${port}`
+    console.log(chalk.green("Server is up and running on PORT = " + chalk.cyan(`${port}`))
     ));
