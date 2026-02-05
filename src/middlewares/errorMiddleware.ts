2072de494427586cf4b72c467e046501bf6168f8
Marcelo R. Barbosa <marceloribeirobarbosa92@gmail.com>
2026-02-05 13:03:25 -0300

    feat: errorHandleMiddleware

---
+++ src/middlewares/errorMiddleware.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+
+const ERRORS = {
+    unauthorized: 401,
+    conflict: 409,
+    not_found: 404,
+    bad_request: 400
+} as const
+
+type ApplicationError = {
+    type: keyof typeof ERRORS;
+    message?:string
+}
+
+export default function errorHandlerMiddleware(err:ApplicationError, req: Request, res: Response, next: NextFunction){
+    const statusCode = ERRORS[err.type] ?? 500;
+    return res.sendStatus(statusCode);
+}
