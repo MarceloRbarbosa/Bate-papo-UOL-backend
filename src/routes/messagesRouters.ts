12b42958feb17dcfef8c9370e0badcc683330d60
Marcelo R. Barbosa <marceloribeirobarbosa92@gmail.com>
2026-02-05 23:31:26 -0300

    feat: message Schema

--- src/routes/messagesRouters.ts
+++ src/routes/messagesRouters.ts
@@ -1,10 +1,12 @@
 import { Router } from "express"
 import messagesController from "../controllers/messagesController"
 import { authenticateToken } from "../middlewares/tokenMiddleware"
+import { validateSchema } from "../middlewares/validateSchemaMiddleware";
+import { createMessageSchema } from "../schemas/messagesSchema";
 
 const messageRouter = Router();
 
-messageRouter.post("/chat", authenticateToken, messagesController.postMessage)
+messageRouter.post("/chat", authenticateToken,validateSchema(createMessageSchema), messagesController.postMessage)
 messageRouter.get("/chat", authenticateToken, messagesController.getMessages)
 
 export default messageRouter
---
+++ src/schemas/messagesSchema.ts
@@ -0,0 +1,7 @@
+import Joi from "joi";
+
+export const createMessageSchema = Joi.object({
+  to: Joi.string().allow("").optional(),
+  text: Joi.string().min(1).required(),
+  type: Joi.string().valid("message", "private_message").required(),
+});
