b9f51dad1bbcab7cd78b15d45497c31c2a2a85b3
Marcelo R. Barbosa <marceloribeirobarbosa92@gmail.com>
2026-02-05 21:35:48 -0300

    feat: add messages Repository

---
+++ src/repositories/messagesRepository.ts
@@ -0,0 +1,19 @@
+import prisma from "../config/database"
+
+async function createMessage(userId:number, to: string, text: string, type:string) {
+    return prisma.message.create({
+        data:{ userId, to, text, type },
+    })
+}
+
+async function listMessages(limit = 80) {
+    return prisma.message.findMany({
+        orderBy: { createdAt: "desc"},
+        take: limit,
+        include:{ user: {select: { id:true, username:true}}},
+    });
+}
+
+const messagesRepository = { createMessage, listMessages}
+
+export default messagesRepository;
